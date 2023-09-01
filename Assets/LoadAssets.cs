4e66ad6018dbcce54f32da6dd9f9a7e6db1301aa
Joshua Dinkin <Joshua.S.Dinkin@student.uts.edu.au>
2023-09-01 17:27:29 +1000

    Push 60 percent

--- Assets/LoadAssets.cs
+++ Assets/LoadAssets.cs
@@ -2,10 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LoadAsset : MonoBehaviour
+public class LoadAssets : MonoBehaviour
 {
     public GameObject redObj;
-    
     // Start is called before the first frame update
     void Start()
     {
