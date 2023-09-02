5782ac58d7bef17eada11039bf3045ff0c82845d
Joshua Dinkin <Joshua.S.Dinkin@student.uts.edu.au>
2023-09-02 22:57:07 +1000

    100 percent

--- Assets/PrintAndHide.cs
+++ Assets/PrintAndHide.cs
@@ -1,21 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class PrintAndHide : MonoBehaviour
 {
     public Renderer rend;
-    private int i = 4;
+    private int BlueRange;
+    private int i = 3;
     // Start is called before the first frame update
     void Start()
     {
-        
+      BlueRange = Random.Range(150, 251);  
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(gameObject.name + ":" + i);
         i++;
+        Debug.Log(gameObject.name + ":" + i);
+        if (gameObject.CompareTag("Red") && i == 100)
+        {
+            gameObject.SetActive(false);
+        }
+        else if (gameObject.CompareTag("Blue") && i == BlueRange)
+        {
+            if (rend != null)
+            {
+                rend.enabled = false;
+            }
+        }
     }
 }
